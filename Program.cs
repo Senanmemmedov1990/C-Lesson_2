30139fa0152b89500e58810abc5b57c7344c1a90
Senanmemmedov1990 <memmedov8665341@gmail.com>
2023-11-03 03:31:35 +0400

    Reverse

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            string sum = null;
+            text = Console.ReadLine();
+            for (int i = text.Length -1; i >= 0; i--)
+            {
+                sum += text[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
